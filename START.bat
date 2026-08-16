a01f5801c61a5d203550bfc322a82378f071f0b2
1314chenjay-star <1314chenjay@gmail.com>
2026-08-16 14:36:47 +0800

    Label Coupang launcher V1.4

--- START.bat
+++ START.bat
@@ -2,7 +2,7 @@
 chcp 65001 >nul
 set "ROOT=%~dp0"
 
-title TinySnow Coupang 數據採集工具 V1
+title TinySnow Coupang 數據採集工具 V1.4
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%ROOT%_system\start\coupang_only.ps1"
 if errorlevel 1 (
   echo.
