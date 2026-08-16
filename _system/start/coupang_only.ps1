3e4cb1af1a8b447475fb1c0fe2bbd1e6cb1a138e
1314chenjay-star <1314chenjay@gmail.com>
2026-08-16 14:00:06 +0800

    Add Coupang-only launcher

---
+++ _system/start/coupang_only.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'
+[Console]::OutputEncoding = [Text.Encoding]::UTF8
+$host.UI.RawUI.WindowTitle = 'TinySnow Coupang 數據採集工具 V1'
+
+. (Join-Path $PSScriptRoot 'coupang_local_collector.ps1')
+
+Show-CoupangCollectorMenuV1
